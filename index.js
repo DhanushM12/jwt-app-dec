27075833f3c944b2bf3517cbc91ae00e4494db73
DhanushM12 <dhanushmukhi@gmail.com>
2022-08-16 21:44:10 +0530

    added verification

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post('/tokenGeneration', (req, res) => {
     username: 'decbatch',
     email: 'dec@gmail.com'
   }
-  jwt.sign(user, 'secretkey', function(err, token) {
+  jwt.sign(user, 'secretkey', {expiresIn: '60s'}, function(err, token) {
     if(err){
       res.statusCode(403)
     }
@@ -26,6 +26,36 @@ app.post('/tokenGeneration', (req, res) => {
     }
   });
 })
+
+
+app.post('/verifyToken', extractToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', function(err, data) {
+    if(err){
+      res.sendStatus(403);
+    }
+    else{
+      res.json({
+        message: 'user access granted',
+        data
+      })
+    }
+  });
+   
+
+})
+// middleware
+function extractToken(req, res, next){
+  const bearerHeader = req.headers['authorization']; // Bearer token
+  if(bearerHeader !== undefined){
+    const bearer = bearerHeader.split(' '); // [bearer, token]
+    const bearerToken = bearer[1]; // token
+    req.token = bearerToken;
+    next();
+  }
+  else{
+    res.sendStatus(403);
+  }
+}
 
 app.listen(port, function(err){
     if(err){
